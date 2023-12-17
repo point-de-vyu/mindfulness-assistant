ac86e0c74311d7890dec3402e9af06fa205691e4
Anastasia Vyunova <anastasia.vyu@gmail.com>
2023-12-17 11:15:24 +0300

    [fix] echo sql

--- api_backend/app/utils.py
+++ api_backend/app/utils.py
@@ -19,22 +19,22 @@ def get_postgres_engine(db_name: str | None = None, is_autocommit: bool = False)
     db_endpoint, main_db_name, db_username, db_password = get_db_connection_parameters()
     db_name = db_name or main_db_name
     engine = sqlalchemy.create_engine(
-        f'postgresql+psycopg2://{db_username}:{db_password}@{db_endpoint}/{db_name}'
+        f'postgresql+psycopg2://{db_username}:{db_password}@{db_endpoint}/{db_name}', echo=True
     )
     if is_autocommit:
         engine = engine.execution_options(autocommit=True)
     return engine
 
 
-def raise_404_error(msg: str ="Not found") -> None:
+def raise_404_error(msg: str = "Not found") -> None:
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail=msg
     )
 
 
-def raise_400_error(msg: str ="Invalid parameter") -> None:
+def raise_400_error(msg: str = "Invalid parameter") -> None:
     raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=msg
-        )
+    )
