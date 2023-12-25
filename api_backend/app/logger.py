1d96cd4413f4a2a4721643b4827bc168b5502bdc
Anastasia Vyunova <anastasia.vyu@gmail.com>
2023-12-25 17:29:29 +0300

    [fix] return emptying handlers to avoid multiple log outputs

--- api_backend/app/logger.py
+++ api_backend/app/logger.py
@@ -7,8 +7,8 @@ main_logger.setLevel(logging.DEBUG)
 
 def set_up_logger(set_up_stdout: bool = True, set_up_file: bool = False) -> logging.Logger:
     global main_logger
-    # main_logger.handlers = []
-    formatter = logging.Formatter("[%(asctime)s] ->%(levelname)s: %(message)s", "%Y-%m-%d %H:%M:%S")
+    main_logger.handlers = []
+    formatter = logging.Formatter("[%(asctime)s] !%(name)s! ->%(levelname)s: %(message)s", "%Y-%m-%d %H:%M:%S")
     if set_up_stdout:
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(formatter)
