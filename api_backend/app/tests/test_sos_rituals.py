d25ff251551619e04a4d8a0677035643a1b48fb5
Anastasia Vyunova <anastasia.vyu@gmail.com>
2023-12-26 21:57:05 +0300

    [feat] new tests

--- api_backend/app/tests/test_sos_rituals.py
+++ api_backend/app/tests/test_sos_rituals.py
@@ -79,3 +79,30 @@ def test_get_default_rituals_with_correct_combo(api: AssistantApi, category: str
 def test_get_default_rituals_with_wrong_combo(api: AssistantApi, category: str, situation: str) -> None:
     response = api.get_with_auth(url=f"/v1/sos_defaults/?category={category}&situation={situation}")
     assert response.status_code == 400
+
+
+def test_add_ok_default_ritual_to_user(api: AssistantApi) -> None:
+    get_user_rits_response = api.get_with_auth("/v1/sos_rituals/")
+    assert get_user_rits_response.status_code == 200
+    # might be []
+    assert get_user_rits_response.json() is not None
+    prev_numb_of_rits = len(get_user_rits_response.json())
+
+    correct_id = 3000129241702033816
+    post_response = api.post_with_auth(url=f"/v1/default_sos_ritual/?default_ritual_id={correct_id}")
+    assert post_response.status_code == 200
+
+    get_user_rits_response_after_post = api.get_with_auth("/v1/sos_rituals/")
+    assert get_user_rits_response_after_post.status_code == 200
+    assert get_user_rits_response_after_post.json() is not None
+    new_len_of_rits = len(get_user_rits_response_after_post.json())
+    assert new_len_of_rits - prev_numb_of_rits == 1
+
+    delete_response = api.delete_with_auth(url=f"/v1/sos_rituals/?ritual_id={correct_id}")
+    assert delete_response.status_code == 200
+
+
+def test_add_wrong_default_ritual_to_user(api: AssistantApi) -> None:
+    wrong_id = 2000129241702033816
+    post_response = api.post_with_auth(url=f"/v1/default_sos_ritual/?default_ritual_id={wrong_id}")
+    assert post_response.status_code == 400
