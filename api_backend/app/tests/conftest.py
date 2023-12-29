b4454a9fbb3e38dfa88a9a369116395d7fc244e3
Anastasia Vyunova <anastasia.vyu@gmail.com>
2023-12-29 17:04:46 +0300

    [feat] using just a db  connection (not yoyo migr) to reset before tests

--- api_backend/app/tests/conftest.py
+++ api_backend/app/tests/conftest.py
@@ -3,23 +3,35 @@ import pytest
 from fastapi.testclient import TestClient
 
 from api_backend.app.utils.db import get_postgres_engine
+from api_backend.app.schemes.sos_rituals import SosTable
 import sqlalchemy
 from requests import Response
+
+
+def get_postgres_engine_for_testing() -> sqlalchemy.engine.Engine:
+    test_db_name = os.environ["TEST_DB_NAME"]
+    return get_postgres_engine(db_name=test_db_name)
 
 
 class AssistantApi(TestClient):
     auth_token: str
 
     """
-    Added authenticated methods. This api uses default testing token.
+    Added authenticated methods and DB resetting upon inition.
+    This api uses default testing user.
     If necessary, each method can take a different auth_token
     """
 
-    def _get_default_auth_token(self):
-        return os.environ["TEST_USER_AUTH_TOKEN"]
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        engine = get_postgres_engine_for_testing()
+        with engine.connect() as connection:
+            self._reset_db(connection)
+            self._add_default_test_user(connection)
+        connection.close()
 
     def _get_auth_headers(self, token: str | None = None):
-        token = token or self._get_default_auth_token()
+        token = token or self.auth_token
         return {"assist-auth": token}
 
     def get_with_auth(self, url: str, auth_token: str | None = None) -> Response:
@@ -39,26 +51,30 @@ class AssistantApi(TestClient):
         )
 
     @staticmethod
-    def reset_db() -> None:
-        # test_db_name = os.environ["TEST_DB_NAME"]
-        # url = get_db_url(db_name=test_db_name)
-        # os.system(f"yoyo apply --database {url} ./yoyo-migrations -b")
-        url = "postgresql://postgres:dsfsdg@db:5432/unittest_db"
-        os.system(f"yoyo reapply --database {url} ./api_backend/app/tests/yoyo-migrations -b")
+    def _reset_db(connection: sqlalchemy.Connection) -> None:
+        """
+        Deleting from users table will cascade to all tables where user_id is a FK.
+        """
+        connection.execute(sqlalchemy.text("DELETE FROM users;"))
+        connection.execute(sqlalchemy.text(f"DELETE FROM {SosTable.RITUALS} "
+                                           f"WHERE id NOT IN (SELECT id FROM {SosTable.DEFAULT_IDS});")
+                           )
+        connection.commit()
 
-
-
-def get_postgres_engine_for_testing() -> sqlalchemy.engine.Engine:
-    test_db_name = os.environ["TEST_DB_NAME"]
-    return get_postgres_engine(db_name=test_db_name)
-
-
-# @pytest.fixture(scope="session", autouse=True)
-# def reset_db() -> None:
-#     test_db_name = os.environ["TEST_DB_NAME"]
-#     # url = get_db_url(db_name=test_db_name)
-#     url = "postgresql://postgres:dsfsdg@localhost:5432/unittest_db"
-#     os.system(f"yoyo reapply --database {url} ./api_backend/app/tests/yoyo-migrations -b")
+    def _add_default_test_user(self, connection: sqlalchemy.Connection) -> None:
+        username = os.environ["TEST_USERNAME"]
+        first_name = os.environ["TEST_FIRSTNAME"]
+        last_name = os.environ["TEST_LASTNAME"]
+        token = os.environ["TEST_USER_AUTH_TOKEN"]
+        connection.execute(sqlalchemy.func.add_new_user(
+            username,
+            first_name,
+            last_name,
+            token
+            )
+        )
+        connection.commit()
+        self.auth_token = token
 
 
 @pytest.fixture()
@@ -67,8 +83,6 @@ def api():
     api = AssistantApi(app=app)
     # override dependency to use test DB
     app.dependency_overrides[get_postgres_engine] = get_postgres_engine_for_testing
-    # use yoyo migration to reset db
-    api.reset_db()
     yield api
 
 
