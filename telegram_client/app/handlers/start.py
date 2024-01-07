f6fb85e717b05df72267b28cf80fec98d9fbbb14
Anastasia Vyunova <anastasia.vyu@gmail.com>
2024-01-07 20:05:29 +0300

    [fix] remove mock storage

--- telegram_client/app/handlers/start.py
+++ telegram_client/app/handlers/start.py
@@ -2,7 +2,7 @@ from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, ReplyKeyboardRemove
-from telegram_client.app.utils.mock_storage import MockStorage
+import random
 
 router = Router()
 
@@ -13,13 +13,12 @@ async def greet_on_start(message: Message, state: FSMContext):
     user_id = user.id
     name = user.first_name
     username = user.username
-    storage = MockStorage()
-    existing_user = storage.get_user(user_id)
+    # TODO make real storage
+    existing_user = random.choice([True, False])
     if existing_user:
         greeting_text = f"Hello {name if name else username}. Good to see you again!"
         function_text = "No need to press start anymore. You can use the menu to navigate ;)"
     else:
-        storage.add_user(user_id, username)
         greeting_text = f"Hello {name if name else username}. Glad to meet you!"
         function_text = f"You can see the commands I can perform in the menu. Remember that " \
                         f"I'm work in progress and hopefully I'll grow big and cool. I'll do my best to help you."
