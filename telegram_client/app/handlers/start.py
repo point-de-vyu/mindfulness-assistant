9103ff6937fcd96d693b2b8d698bb7e514a94339
Anastasia Vyunova <anastasia.vyu@gmail.com>
2024-03-08 20:24:07 +0300

    [fix] remove states from params

--- telegram_client/app/handlers/start.py
+++ telegram_client/app/handlers/start.py
@@ -1,14 +1,15 @@
 from aiogram import F, Router
 from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram_client.app.handlers.sign_up import explain_signing_up
+
+import logging
 
 router = Router()
 
 
 @router.message(Command("start"))
-async def greet_on_start(message: Message, state: FSMContext):
+async def greet_on_start(message: Message):
     user = message.from_user
     name = user.first_name
     username = user.username
@@ -16,10 +17,10 @@ async def greet_on_start(message: Message, state: FSMContext):
     intro = f"I'm designed to help you with your mindfulness and mental health."
     await message.answer(text=greeting)
     await message.answer(text=intro)
-    await skills(message, state)
+    await skills(message)
 
 
-async def skills(message: Message, state: FSMContext):
+async def skills(message: Message):
     text1 = "At the moment, you can call a /sos command if you feel overwhelmed, and I'll offer you an exercise to " \
            "try to deal with your feelings."
     await message.answer(text=text1)
