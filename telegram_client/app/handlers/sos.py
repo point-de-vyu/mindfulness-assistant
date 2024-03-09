3bd80a3ae81a09d1ee35aafd9465a1580708613c
Anastasia Vyunova <anastasia.vyu@gmail.com>
2024-03-09 13:40:59 +0300

    [feat] added keyboards, correct working version with correct buttons

--- telegram_client/app/handlers/sos.py
+++ telegram_client/app/handlers/sos.py
@@ -2,8 +2,7 @@ from aiogram import F, Router
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-from aiogram.types import CallbackQuery, Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, \
-    InlineKeyboardMarkup
+from aiogram.types import CallbackQuery, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 from telegram_client.app.utils.requests import get_headers
 import requests
 import os
@@ -60,6 +59,7 @@ async def get_rituals(
     response = requests.get(url=url, headers=headers)
     status_code = response.status_code
 
+    # TODO return {code: int, res: []} - check for this code in handler and go to error from there
     if status_code != 200:
         await error(message)
         return
@@ -74,7 +74,7 @@ async def get_rituals_for_situation(message: Message):
     situation = message.text
     await add_data_to_storage({"sos_situation": situation}, message.from_user.id)
     rituals = await get_rituals(message, situation=situation)
-    buttons = [[KeyboardButton(text="Journal you feelings after this exercise")]]
+    buttons = [[KeyboardButton(text="Journal your feelings after this exercise")]]
     if not rituals:
         default_rituals = await get_rituals(message, is_default=True, situation=situation)
         if not default_rituals:
@@ -85,6 +85,8 @@ async def get_rituals_for_situation(message: Message):
             text=f"You haven't added or created your {situation.lower()} rituals. Here's my suggestion:"
         )
         buttons.append([KeyboardButton(text="Add to favourites")])
+        await add_data_to_storage({"is_default_last_ritual": True}, message.from_user.id)
+    # TODO вот если здесь сразу один, мы не предложим посмотреть дефолтные
     current_ritual = rituals.pop()
     available_categories = list(set([ritual["category"] for ritual in rituals]))
     buttons.extend([[KeyboardButton(text=cat) for cat in available_categories]])
@@ -97,22 +99,39 @@ async def get_rituals_for_situation(message: Message):
     )
 
 
+# TODO better names for buttons "another X"? and input understanding (now 400 for lowercase)
 @router.message(F.text.lower().in_({"meditation", "affirmation", "breathing exercise"}))
 async def get_ritual_for_category(message: Message):
-    # мб лучше не запоминать, а тоже стучаться в дб, а потом проверять, что айдишник не равен текущему
-    # но в этом случае будем бесконечно повторять все доступные, и можем случайно предложить предыдущее
     category = message.text
     rituals = await extract_data_from_storage("available_rituals", message.from_user.id)
     if not rituals:
-        await message.answer(text="I'm afraid there's no more")
+        await message.answer(
+            text="I'm afraid there's no more. Use the command to restart this flow",
+            reply_markup=ReplyKeyboardRemove()
+        )
         return
     buttons = [
-        [KeyboardButton(text="Journal you feelings after the exercise")]]
+        [KeyboardButton(text="Journal your feelings after the exercise")]]
 
     current_ritual = [ritual for ritual in rituals if ritual["category"] == category][0]
     rituals.remove(current_ritual)
-    available_categories = list(set([ritual["category"] for ritual in rituals]))
-    buttons.extend([[KeyboardButton(text=cat) for cat in available_categories]])
+    # TODO to bool shown_defaults now is True or ""
+    shown_defaults = await extract_data_from_storage("is_default_last_ritual", message.from_user.id)
+    if shown_defaults:
+        buttons.append([KeyboardButton(text="Add to favourites")])
+    if len(rituals) == 0 and not shown_defaults:
+        situation = await extract_data_from_storage("sos_situation", message.from_user.id)
+        default_rituals = await get_rituals(message, is_default=True, situation=situation, category=category)
+        user_rituals = await get_rituals(message, is_default=False, situation=situation, category=category)
+        user_ritual_ids = [rit["id"] for rit in user_rituals]
+        rituals = [rit for rit in default_rituals if rit["id"] not in user_ritual_ids]
+
+    if rituals:
+        if not shown_defaults:
+            buttons.extend([[KeyboardButton(text="Show suggested rituals")]])
+        else:
+            available_categories = list(set([ritual["category"] for ritual in rituals]))
+            buttons.extend([[KeyboardButton(text=cat) for cat in available_categories]])
 
     await add_data_to_storage({"current_ritual": current_ritual, "available_rituals": rituals}, message.from_user.id)
     await message.answer(
